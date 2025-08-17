80e770b9aebdf13224a1a3507b8ae8d99451f8dc
johnthedy <109345089+johnthedy@users.noreply.github.com>
2025-08-17 08:01:56 +0700

    Update SAPSIMULATION.m

--- SAPSIMULATION.m
+++ SAPSIMULATION.m
@@ -1,30 +1,22 @@
-function [W,RecRatio,BARSCHEDULE,recpattern]=SAPSIMULATION(sparam,gparam,DL,LL,EQset,Wlist,Clist,Plist)
-
-BARSCHEDULE=[];
+function [RecRatio,idxframe]=SAPSIMULATION(sparam,gparam,DL,LL,WL,EQset,Wlist,Clist,Plist)
 
 Req=EQset(1);
-Omega=EQset(2);
-Cd=EQset(3);
-Ss=EQset(4);
-S1=EQset(5);
-Ct=EQset(6);
-x=EQset(7);
+Ss=EQset(2);
+S1=EQset(3);
+Ct=EQset(4);
+x=EQset(5);
 
 %% Geometry Parameter
 %gparam1=warehouse length, gparam2=warehouse width,
 %gparam3=warehouse column height, gparam4=warehouse total height
 %gparam5=number of portal, gparam6=purlin spacing
-gparam1=gparam(1);
-gparam2=gparam(2);
+gparam1=round(gparam(1)/100)*100;
+gparam2=round(gparam(2)/100)*100;
 gparam3=round(gparam(3)/100)*100;
-gparam4=gparam(4);
+gparam4=round(gparam(4)/100)*100;
 gparam5=gparam(5);
 gparam6=gparam(6);
-gparam7=round(gparam(7)/100)*100;
 
-if sparam(end)<=0.15
-    sparam(end)=0;
-end
 %% Section Parameter
 %Column WF Section
 if sparam(1)<sparam(2)*1.25
@@ -37,7 +29,6 @@ sparam1=Wlist(d3,2);
 sparam2=Wlist(d3,4);
 sparam3=Wlist(d3,5);
 sparam4=Wlist(d3,3);
-COLNAMEIDX=d3;
 
 %Girder WF Section
 d1=sparam(2);
@@ -47,7 +38,6 @@ sparam5=Wlist(d3,2);
 sparam6=Wlist(d3,4);
 sparam7=Wlist(d3,5);
 sparam8=Wlist(d3,3);
-GIRNAMEIDX=d3;
 
 %Tie Beam WF Section
 d1=sparam(3);
@@ -57,7 +47,6 @@ sparam9=Wlist(d3,2);
 sparam10=Wlist(d3,4);
 sparam11=Wlist(d3,5);
 sparam12=Wlist(d3,3);
-TIENAMEIDX=d3;
 
 %Purlin
 d1=sparam(4);
@@ -67,7 +56,6 @@ sparam13=Clist(d3,2);
 sparam14=Clist(d3,4);
 sparam15=Clist(d3,5);
 sparam16=Clist(d3,3);
-PURNAMEIDX=d3;
 
 %Bracing
 d1=sparam(5);
@@ -75,7 +63,6 @@ d2=abs(Plist(:,3)-d1);
 [~,d3]=min(d2);
 sparam17=Plist(d3,4);
 sparam18=Plist(d3,4)-Plist(d3,5);
-BRCNAMEIDX=d3;
 
 %% Preparation
 fpath=sprintf('%s\\Structure\\',pwd);
@@ -119,7 +106,7 @@ ConstraintDef = NET.explicitCast(SapModel.ConstraintDef,'SAP2000v1.cConstraint')
 AISC360_10 = NET.explicitCast(DesignSteel.AISC360_10,'SAP2000v1.cDStAISC360_10');
 
 SapObject.ApplicationStart;
-SapObject.Hide;
+% SapObject.Hide;
 SapModel.SetPresentUnits(SAP2000v1.eUnits.N_mm_C);
 File.NewBlank;
 
@@ -127,7 +114,7 @@ File.NewBlank;
 %Column Coordinate
 CCoord=[];
 Cx=[0 gparam2];
-Cy=[0 round(linspace(gparam7,gparam1,gparam5-1))];
+Cy=[round(linspace(0,gparam1,gparam5-1))];
 Cz=[0 gparam3];
 for i=1:length(Cx)
     for j=1:length(Cy)
@@ -147,7 +134,7 @@ d4=round(d1.*d22);
 
 Gx=sort([d3 gparam2-d3(1:end-1)]);
 Gz=[d4 fliplr(d4(1:end-1))]+gparam3;
-Gy=[0 round(linspace(gparam7,gparam1,gparam5-1))];
+Gy=[round(linspace(0,gparam1,gparam5-1))];
 
 for i=1:length(Gy)
     for j=1:length(Gx)
@@ -184,18 +171,6 @@ for i=1:length(d1)
     idxframe=vertcat(idxframe,{'COL',idx});
     idx=idx+1;
 end
-nCOL=length(d1);
-lCOL=gparam3;
-if lCOL<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 COLNAMEIDX nCOL lCOL]);
-else
-    d1=lCOL;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[1 COLNAMEIDX nCOL 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 COLNAMEIDX nCOL d1]);
-end
 
 %% Tie Beam Setup
 d1=find(CCoord(:,1)==0 & CCoord(:,3)==gparam3);
@@ -210,30 +185,6 @@ for i=1:length(d2)-1
     FrameObj.AddByCoord(CCoord(d2(i),1),CCoord(d2(i),2),CCoord(d2(i),3),CCoord(d2(i+1),1),CCoord(d2(i+1),2),CCoord(d2(i+1),3),'','TIE');
     idxframe=vertcat(idxframe,{'TIE',idx});
     idx=idx+1;
-end
-nTIE1=2;
-lTIE1=gparam7;
-if lTIE1<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 TIENAMEIDX nTIE1 lTIE1]);
-else
-    d1=lTIE1;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[1 TIENAMEIDX nTIE1 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 TIENAMEIDX nTIE1 d1]);
-end
-nTIE2=2*(gparam5-2);
-lTIE2=(gparam1-gparam7)/(gparam5-2);
-if lTIE2<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 TIENAMEIDX nTIE2 lTIE2]);
-else
-    d1=lTIE2;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[1 TIENAMEIDX nTIE2 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 TIENAMEIDX nTIE2 d1]);
 end
 
 %% Girder Setup
@@ -260,24 +211,6 @@ for i=d2+1:d2+d3-length(d1)
     PointObj.SetConstraint(sprintf("%d",i), "DIAPH1");
 end
 
-nGIR=gparam5*2;
-lGIR=sqrt((gparam2/2)^2+(gparam4-gparam3)^2);
-if sparam(end)~=0
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 GIRNAMEIDX nGIR sparam(end)*lGIR]);
-    lGIR=lGIR-sparam(end)*lGIR;
-end
-if lGIR<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 GIRNAMEIDX nGIR lGIR]);
-else
-    d1=lGIR;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[1 GIRNAMEIDX nGIR 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[1 GIRNAMEIDX nGIR d1]);
-end
-BARSCHEDULE=vertcat(BARSCHEDULE,[1 GIRNAMEIDX nGIR 0.1*lGIR]);
-
 %% Purlin Setup
 d1=unique(GCoord(:,2));
 for i=1:length(d1)-1
@@ -297,32 +230,6 @@ StartValue=zeros(6,1);
 EndValue=zeros(6,1);
 for i=1:length(d1)
     FrameObj.SetReleases(sprintf('%d',d1(i)), ii, jj, StartValue, EndValue);
-end
-
-nPUR1=(gparam6*2);
-lPUR1=gparam7;
-if lPUR1<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[2 PURNAMEIDX nPUR1 lPUR1]);
-else
-    d1=lPUR1;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[2 PURNAMEIDX nPUR1 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[2 PURNAMEIDX nPUR1 d1]);
-end
-
-nPUR2=(gparam6*2)*(gparam5-2);
-lPUR2=(gparam1-gparam7)/(gparam5-2);
-if lPUR2<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[2 PURNAMEIDX nPUR2 lPUR2]);
-else
-    d1=lPUR2;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[2 PURNAMEIDX nPUR2 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[2 PURNAMEIDX nPUR2 d1]);
 end
 
 %% Bracing Setup
@@ -372,30 +279,6 @@ for j=1:length(d2)-1
     idxframe=vertcat(idxframe,{'BRC',idx});
     idx=idx+1;
 end
-nBRC1=8;
-lBRC1=round(sqrt(gparam7^2+gparam3^2))/2;
-if lBRC1<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[3 BRCNAMEIDX nBRC1 lBRC1]);
-else
-    d1=lBRC1;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[3 BRCNAMEIDX nBRC1 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[3 BRCNAMEIDX nBRC1 d1]);
-end
-nBRC2=8*(gparam5-2);
-lBRC2=round(sqrt(((gparam1-gparam7)/(gparam5-2))^2+gparam3^2))/2;
-if lBRC2<=12000
-    BARSCHEDULE=vertcat(BARSCHEDULE,[3 BRCNAMEIDX nBRC2 lBRC2]);
-else
-    d1=lBRC2;
-    while d1>12000
-        BARSCHEDULE=vertcat(BARSCHEDULE,[3 BRCNAMEIDX nBRC2 12000]);
-        d1=d1-12000;
-    end
-    BARSCHEDULE=vertcat(BARSCHEDULE,[3 BRCNAMEIDX nBRC2 d1]);
-end
 
 %% Set Restraint
 d1=find(CCoord(:,3)==0);
@@ -417,8 +300,6 @@ LoadPatterns.Add('WINDY',SAP2000v1.eLoadPatternType.Wind,0,true);
 SourceMass.SetMassSource('MSSSRC1',false,false,true,true,2,{'DEAD','LIVE'},[1 0.5]);
 
 %% Assign Load
-[pwind1,pwind2]=WindLoad(gparam1,gparam2,gparam3,gparam4,gparam5,gparam6,gparam7);
-
 for i=1:length(Cy)
     if i==1
         LoadLength(i)=(Cy(i+1)-Cy(i))/2;
@@ -432,31 +313,17 @@ LoadLength=LoadLength./1000;
 % Unit in N/mm
 UDDL=DL.*LoadLength.*9.8./1000;
 UDLL=LL.*LoadLength.*9.8./1000;
+UDWY=WL.*LoadLength.*9.8./1000;
 
 d2=find(strcmp(idxframe(:,1),'GIR'));
-d3=1:length(d2)/gparam5:length(d2);
+d3=1:length(d2)/(gparam5-1):length(d2);
 
 for i=1:length(d3)
-    for j=d3(i):d3(i)+length(d2)/gparam5-1
+    for j=d3(i):d3(i)+length(d2)/(gparam5-1)-1
         FrameObj.SetLoadDistributed(sprintf('%d',d2(j)), "DEAD", 1, 10, 0, 1, UDDL(i), UDDL(i));
         FrameObj.SetLoadDistributed(sprintf('%d',d2(j)), "LIVE", 1, 10, 0, 1, UDLL(i), UDLL(i));
-        if j<((d3(i)+length(d2)/gparam5-1)+d3(i))/2
-            UDWY=pwind1*LoadLength(i)/1000;
-        else
-            UDWY=-pwind2*LoadLength(i)/1000;
-        end
-        FrameObj.SetLoadDistributed(sprintf('%d',d2(j)), "WINDY", 1, 7, 0, 1, UDWY, UDWY);
+        FrameObj.SetLoadDistributed(sprintf('%d',d2(j)), "WINDY", 1, 4, 0, 1, UDWY(i), UDWY(i));
     end
-end
-
-% d1=sqrt((gparam2/2)^2+(gparam4-gparam3)^2)./gparam6;
-% LoadLength=d1./1000;
-% UDDL=DL.*LoadLength.*9.8./1000;
-UDLL=60*9.8./1000;
-d2=find(strcmp(idxframe(:,1),'PUR'));
-for i=1:length(d2)
-    % FrameObj.SetLoadDistributed(sprintf('%d',d2(i)), "DEAD", 1, 10, 0, 1, UDDL, UDDL);
-    FrameObj.SetLoadDistributed(sprintf('%d',d2(i)), "LIVE", 1, 10, 0, 1, UDLL, UDLL);
 end
 
 %% EQ Param
@@ -607,54 +474,18 @@ RespCombo.SetCaseList('COMB4',SAP2000v1.eCNameType.LoadCase,'LIVE',0.5);
 RespCombo.SetCaseList('COMB4',SAP2000v1.eCNameType.LoadCombo,'EQ1',-1.0);
 RespCombo.SetCaseList('COMB4',SAP2000v1.eCNameType.LoadCombo,'EV',1.0);
 RespCombo.Add('COMB5',0);
-RespCombo.SetCaseList('COMB5',SAP2000v1.eCNameType.LoadCase,'DEAD',1.2);
-RespCombo.SetCaseList('COMB5',SAP2000v1.eCNameType.LoadCase,'LIVE',0.5);
-RespCombo.SetCaseList('COMB5',SAP2000v1.eCNameType.LoadCase,'DX',1.0);
-RespCombo.SetCaseList('COMB5',SAP2000v1.eCNameType.LoadCombo,'EV',1.0);
-RespCombo.Add('COMB6',0);
-RespCombo.SetCaseList('COMB6',SAP2000v1.eCNameType.LoadCase,'DEAD',1.2);
-RespCombo.SetCaseList('COMB6',SAP2000v1.eCNameType.LoadCase,'LIVE',0.5);
-RespCombo.SetCaseList('COMB6',SAP2000v1.eCNameType.LoadCase,'DX',-1.0);
-RespCombo.SetCaseList('COMB6',SAP2000v1.eCNameType.LoadCombo,'EV',1.0);
-RespCombo.Add('COMB7',0);
-RespCombo.SetCaseList('COMB7',SAP2000v1.eCNameType.LoadCase,'DEAD',1.2);
-RespCombo.SetCaseList('COMB7',SAP2000v1.eCNameType.LoadCase,'LIVE',0.5);
-RespCombo.SetCaseList('COMB7',SAP2000v1.eCNameType.LoadCase,'DY',1.0);
-RespCombo.SetCaseList('COMB7',SAP2000v1.eCNameType.LoadCombo,'EV',1.0);
-RespCombo.Add('COMB8',0);
-RespCombo.SetCaseList('COMB8',SAP2000v1.eCNameType.LoadCase,'DEAD',1.2);
-RespCombo.SetCaseList('COMB8',SAP2000v1.eCNameType.LoadCase,'LIVE',0.5);
-RespCombo.SetCaseList('COMB8',SAP2000v1.eCNameType.LoadCase,'DY',-1.0);
-RespCombo.SetCaseList('COMB8',SAP2000v1.eCNameType.LoadCombo,'EV',1.0);
-RespCombo.Add('COMB9',0);
 RespCombo.SetCaseList('COMB9',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
 RespCombo.SetCaseList('COMB9',SAP2000v1.eCNameType.LoadCombo,'EQ1',1.0);
 RespCombo.SetCaseList('COMB9',SAP2000v1.eCNameType.LoadCombo,'EV',-1.0);
-RespCombo.Add('COMB10',0);
+RespCombo.Add('COMB6',0);
 RespCombo.SetCaseList('COMB10',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
 RespCombo.SetCaseList('COMB10',SAP2000v1.eCNameType.LoadCombo,'EQ1',-1.0);
 RespCombo.SetCaseList('COMB10',SAP2000v1.eCNameType.LoadCombo,'EV',-1.0);
-RespCombo.Add('COMB11',0);
-RespCombo.SetCaseList('COMB11',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
-RespCombo.SetCaseList('COMB11',SAP2000v1.eCNameType.LoadCase,'DX',1.0);
-RespCombo.SetCaseList('COMB11',SAP2000v1.eCNameType.LoadCombo,'EV',-1.0);
-RespCombo.Add('COMB12',0);
-RespCombo.SetCaseList('COMB12',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
-RespCombo.SetCaseList('COMB12',SAP2000v1.eCNameType.LoadCase,'DX',-1.0);
-RespCombo.SetCaseList('COMB12',SAP2000v1.eCNameType.LoadCombo,'EV',-1.0);
-RespCombo.Add('COMB13',0);
-RespCombo.SetCaseList('COMB13',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
-RespCombo.SetCaseList('COMB13',SAP2000v1.eCNameType.LoadCase,'DY',1.0);
-RespCombo.SetCaseList('COMB13',SAP2000v1.eCNameType.LoadCombo,'EV',-1.0);
-RespCombo.Add('COMB14',0);
-RespCombo.SetCaseList('COMB14',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
-RespCombo.SetCaseList('COMB14',SAP2000v1.eCNameType.LoadCase,'DY',-1.0);
-RespCombo.SetCaseList('COMB14',SAP2000v1.eCNameType.LoadCombo,'EV',-1.0);
-RespCombo.Add('COMB15',0);
+RespCombo.Add('COMB7',0);
 RespCombo.SetCaseList('COMB15',SAP2000v1.eCNameType.LoadCase,'DEAD',1.2);
 RespCombo.SetCaseList('COMB15',SAP2000v1.eCNameType.LoadCase,'WINDY',1.0);
 RespCombo.SetCaseList('COMB15',SAP2000v1.eCNameType.LoadCombo,'LIVE',1.0);
-RespCombo.Add('COMB16',0);
+RespCombo.Add('COMB8',0);
 RespCombo.SetCaseList('COMB16',SAP2000v1.eCNameType.LoadCase,'DEAD',0.9);
 RespCombo.SetCaseList('COMB16',SAP2000v1.eCNameType.LoadCase,'WINDY',1.0);
 
@@ -677,14 +508,6 @@ DesignSteel.SetComboStrength('COMB5',true);
 DesignSteel.SetComboStrength('COMB6',true);
 DesignSteel.SetComboStrength('COMB7',true);
 DesignSteel.SetComboStrength('COMB8',true);
-DesignSteel.SetComboStrength('COMB9',true);
-DesignSteel.SetComboStrength('COMB10',true);
-DesignSteel.SetComboStrength('COMB11',true);
-DesignSteel.SetComboStrength('COMB12',true);
-DesignSteel.SetComboStrength('COMB13',true);
-DesignSteel.SetComboStrength('COMB14',true);
-DesignSteel.SetComboStrength('COMB15',true);
-DesignSteel.SetComboStrength('COMB16',true);
 DesignSteel.SetComboAutoGenerate(false);
 DesignSteel.StartDesign;
 
@@ -706,77 +529,7 @@ for i=1:length(idxframe)
         RecRatio(i,:)=Ratio(1);
     end
 end
-
-LN='DX';
-AnalysisResultsSetup.DeselectAllCasesAndCombosForOutput;
-AnalysisResultsSetup.SetCaseSelectedForOutput(LN);
-AnalysisResultsSetup.SetOptionDirectHist(2);
-NumberResults = 0;
-Obj = NET.createArray('System.String',2);
-Elm = NET.createArray('System.String',2);
-ACase = NET.createArray('System.String',2);
-StepType = NET.createArray('System.String',2);
-StepNum = NET.createArray('System.Double',2);
-U1 = NET.createArray('System.Double',2);
-U2 = NET.createArray('System.Double',2);
-U3 = NET.createArray('System.Double',2);
-R1 = NET.createArray('System.Double',2);
-R2 = NET.createArray('System.Double',2);
-R3 = NET.createArray('System.Double',2);
-[~, NumberResults, Obj, Elm, ACase, StepType, StepNum, U1, U2, U3, R1, R2, R3] = AnalysisResults.JointDispl('2', SAP2000v1.eItemTypeElm.ObjectElm, NumberResults, Obj, Elm, ACase, StepType, StepNum, U1, U2, U3, R1, R2, R3);
-dummy1=abs(double(U1));
-LN='DY';
-AnalysisResultsSetup.DeselectAllCasesAndCombosForOutput;
-AnalysisResultsSetup.SetCaseSelectedForOutput(LN);
-AnalysisResultsSetup.SetOptionDirectHist(2);
-NumberResults = 0;
-Obj = NET.createArray('System.String',2);
-Elm = NET.createArray('System.String',2);
-ACase = NET.createArray('System.String',2);
-StepType = NET.createArray('System.String',2);
-StepNum = NET.createArray('System.Double',2);
-U1 = NET.createArray('System.Double',2);
-U2 = NET.createArray('System.Double',2);
-U3 = NET.createArray('System.Double',2);
-R1 = NET.createArray('System.Double',2);
-R2 = NET.createArray('System.Double',2);
-R3 = NET.createArray('System.Double',2);
-[~, NumberResults, Obj, Elm, ACase, StepType, StepNum, U1, U2, U3, R1, R2, R3] = AnalysisResults.JointDispl('2', SAP2000v1.eItemTypeElm.ObjectElm, NumberResults, Obj, Elm, ACase, StepType, StepNum, U1, U2, U3, R1, R2, R3);
-dummy2=abs(double(U2));
-EQDrift=Cd*max([dummy1 dummy2])/lCOL;
-
 SapObject.ApplicationExit(false);
-disp(BARSCHEDULE)
-d1=unique(BARSCHEDULE(:,1));d2=unique(BARSCHEDULE(:,2));
-recpattern={};W=0;idxp=1;
-for i=1:length(d1)
-    for j=1:length(d2)
-        d3=find(BARSCHEDULE(:,1)==d1(i));
-        d4=find(BARSCHEDULE(:,2)==d2(j));
-        d5=intersect(d3,d4);
-        if isempty(d5)~=1
-            input1=12000;
-            input2=BARSCHEDULE(d5,4);
-            input3=BARSCHEDULE(d5,3);
-            pattern=CSP1D(input1,input2,input3);
-            recpattern{idxp}=pattern;idxp=idxp+1;
-            if BARSCHEDULE(d5(1),1)==1
-                W=W+sum(pattern(:,end-1))*input1*Wlist(BARSCHEDULE(d5(1),2),6);
-            elseif BARSCHEDULE(d5(1),1)==2
-                W=W+sum(pattern(:,end-1))*input1*Clist(BARSCHEDULE(d5(1),2),6);
-            else
-                W=W+sum(pattern(:,end-1))*input1*Plist(BARSCHEDULE(d5(1),2),2);
-            end
-        end
-    end
-end
-[d1,~]=size(BARSCHEDULE);
-W=W+d1*50;
-W=W./(3e8);
 
-if max(RecRatio)>1 || EQDrift>0.02
-    W=inf;
-    RecRatio=inf;
-end
 
 end
